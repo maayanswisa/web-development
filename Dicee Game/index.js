6d697216ef53a496900059d730c9de73c08d8290
Maayan Swisa <120381527+maayanswisa@users.noreply.github.com>
2024-11-03 13:42:35 +0200

    fix

--- Dicee Game/index.js
+++ Dicee Game/index.js
@@ -10,12 +10,8 @@ document.querySelector(".img2").setAttribute("src", randomDiceImage2);
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerText = "Player 1 Wins!";
-}
-
-if (randomNumber1 < randomNumber2) {
+} else if (randomNumber1 < randomNumber2) {
   document.querySelector("h1").innerText = "Player 2 Wins!";
-}
-
-if (randomNumber1 == randomNumber2) {
+} else {
   document.querySelector("h1").innerText = "It's a Draw!";
 }
