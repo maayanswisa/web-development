dce25abb1448bb493faa68d5f221b51bf48df5a9
Maayan Swisa <120381527+maayanswisa@users.noreply.github.com>
2025-06-10 12:28:16 +0300

    dicee add

--- Dicee Game Challenge/index.js
+++ Dicee Game Challenge/index.js
@@ -9,3 +9,15 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
 //change the right qube
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerText = "Player 1 Wins!";
+}
+
+if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerText = "Player 2 Wins!";
+}
+
+if (randomNumber1 === randomNumber2) {
+  document.querySelector("h1").innerText = "Its A Draw!";
+}
