7849f33f10a4fb14b357231ab76ff2d81eb59bed
Maayan Swisa <120381527+maayanswisa@users.noreply.github.com>
2025-06-19 13:18:21 +0300

    211

--- Backend/4.2 Passind Data/index.js
+++ Backend/4.2 Passind Data/index.js
@@ -6,9 +6,15 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs", { countLetters: 0 });
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const countLetters = req.body.fName.length + req.body.lName.length;
+  console.log(countLetters);
+  res.render("index.ejs", { countLetters: countLetters });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
